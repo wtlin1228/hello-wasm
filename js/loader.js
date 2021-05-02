ecafd058f1307fb3a3977ff5c4b706e168f24390
wtlin1228 <wtlin1228@gmail.com>
2021-05-02 14:34:24 +0800

    use AssemblyScript Loader to replace WebAssembly, add fizzbuzz example

--- js/loader.js
+++ js/loader.js
@@ -6,20 +6,22 @@ class WasmLoader {
           throw new Error('Abort called from wasm file')
         },
       },
+      index: {
+        log(n) {
+          console.log(n)
+        },
+      },
     }
   }
 
   async wasm(path, imports = this._imports) {
     console.log(`Fetching ${path}`)
 
-    if (!WebAssembly.instantiateStreaming) {
+    if (!loader.instantiateStreaming) {
       return this.wasmFallback(path, imports)
     }
 
-    const { instance } = await WebAssembly.instantiateStreaming(
-      fetch(path),
-      imports
-    )
+    const instance = await loader.instantiateStreaming(fetch(path), imports)
 
     return instance?.exports
   }
@@ -29,7 +31,7 @@ class WasmLoader {
 
     const response = await fetch(path)
     const bytes = await response?.arrayBuffer()
-    const { instance } = await WebAssembly.instantiate(bytes, imports)
+    const instance = await loader.instantiate(bytes, imports)
 
     return instance?.exports
   }
